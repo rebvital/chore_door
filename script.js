46eda40f9f440d8eba44d4d659af1d659ce333e7
Rebeca Vital Fernandes <44797156+rebvital@users.noreply.github.com>
2020-10-08 22:27:23 +0100

    first part finished

---
+++ script.js
@@ -0,0 +1,18 @@
+let doorImage1 = document.querySelector("#door1");
+let doorImage2 = document.querySelector("#door2");
+let doorImage3 = document.querySelector("#door3");
+let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg';
+
+doorImage1.onclick = () => {
+    doorImage1.src = botDoorPath;
+}
+
+doorImage2.onclick = () => {
+    doorImage2.src = beachDoorPath;
+}
+
+doorImage3.onclick = () => {
+    doorImage3.src = spaceDoorPath;
+}
